4749e97f2bc419000799a3936d83a38b23f01cc1
MaximvanMaanen <maxim.vanmaanen@gmail.com>
2023-11-19 10:57:11 +0100

    Added: DataSet test for DoublyLinkedList

--- Algorithms/Algorithms.Tests/DoublyLinkedListTests.cs
+++ Algorithms/Algorithms.Tests/DoublyLinkedListTests.cs
@@ -1,9 +1,44 @@
 ﻿using Algorithms.DoublyLinkedList;
+using Algorithms.JsonData;
+using Algorithms.JsonData.Sorteren.Models;
+using Newtonsoft.Json;
 
 namespace Algorithms.Tests
 {
     public class DoublyLinkedListTests
     {
+        [Fact]
+        public void TheDataSets_Fit_InTheDoublyLinkedList()
+        {
+            var sortingJson = JsonConstants.ReadDataSetSorting();
+
+            var result1 = Record.Exception(() => new DoublyLinkedList<int>(JsonConvert.DeserializeObject<LijstAflopend2>(sortingJson).Content));
+            var result2 = Record.Exception(() => new DoublyLinkedList<int>(JsonConvert.DeserializeObject<LijstOplopend2>(sortingJson).Content));
+            var result3 = Record.Exception(() => new DoublyLinkedList<double>(JsonConvert.DeserializeObject<LijstFloat8001>(sortingJson).Content));
+            var result4 = Record.Exception(() => new DoublyLinkedList<int>(JsonConvert.DeserializeObject<LijstGesorteerdAflopend3>(sortingJson).Content));
+            var result5 = Record.Exception(() => new DoublyLinkedList<int>(JsonConvert.DeserializeObject<LijstGesorteerdOplopend3>(sortingJson).Content));
+            var result6 = Record.Exception(() => new DoublyLinkedList<int>(JsonConvert.DeserializeObject<LijstHerhaald1000>(sortingJson).Content));
+            var result7 = Record.Exception(() => new DoublyLinkedList<object>(JsonConvert.DeserializeObject<LijstLeeg0>(sortingJson).Content));
+            var result8 = Record.Exception(() => new DoublyLinkedList<object>(JsonConvert.DeserializeObject<LijstNull1>(sortingJson).Content));
+            var result9 = Record.Exception(() => new DoublyLinkedList<int?>(JsonConvert.DeserializeObject<LijstNull3>(sortingJson).Content));
+            var result10 = Record.Exception(() => new DoublyLinkedList<int>(JsonConvert.DeserializeObject<LijstOplopend10000>(sortingJson).Content));
+            var result11 = Record.Exception(() => new DoublyLinkedList<int>(JsonConvert.DeserializeObject<LijstWillekeurig10000>(sortingJson).Content));
+            var result12 = Record.Exception(() => new DoublyLinkedList<int>(JsonConvert.DeserializeObject<LijstWillekeurig3>(sortingJson).Content));
+
+            Assert.Null(result1);
+            Assert.Null(result2);
+            Assert.Null(result3);
+            Assert.Null(result4);
+            Assert.Null(result5);
+            Assert.Null(result6);
+            Assert.Null(result7);
+            Assert.Null(result8);
+            Assert.Null(result9);
+            Assert.Null(result10);
+            Assert.Null(result11);
+            Assert.Null(result12);
+        }
+
         [Fact]
         public void Add_AddsAnItem_OnTheEnd()
         {
