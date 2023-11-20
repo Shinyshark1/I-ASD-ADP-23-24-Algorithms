1fbce90201ad3e6c6d950fe30e1c168192075558
MaximvanMaanen <maxim.vanmaanen@gmail.com>
2023-11-20 21:45:05 +0100

    Added: DataSetHelper
    Allows us to make some easy data sets real quickly.

---
+++ Algorithms/Algorithms/Shared/DataSetHelper.cs
@@ -0,0 +1,41 @@
+﻿namespace Algorithms.Shared
+{
+    public static class DataSetHelper
+    {
+        /// <summary>
+        /// Creates an array of ints in order from 1 to whatever size you give.
+        /// </summary>
+        /// <param name="size">The size the array should be.</param>
+        public static int[] CreateDataSet(int size)
+        {
+            int[] data = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                data[i] = i + 1;
+            }
+
+            return data;
+        }
+
+        /// <summary>
+        /// Creates an array of random ints.
+        /// </summary>
+        /// <param name="size">The size the array should be.</param>
+        /// <param name="mininumInt">The lowest number you want to exist in your array.</param>
+        /// <param name="maximumInt">The highest number you want to exist in your array.</param>
+        public static int[] CreateRandomDataSet(int size, int mininumInt = 1, int maximumInt = 100)
+        {
+            maximumInt++; // Random takes the maximumInt as upper limit, so to get 100 as max, we have to provide 101.
+            var randomGenerator = new Random();
+
+
+            int[] data = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                data[i] = randomGenerator.Next(mininumInt, maximumInt);
+            }
+
+            return data;
+        }
+    }
+}
