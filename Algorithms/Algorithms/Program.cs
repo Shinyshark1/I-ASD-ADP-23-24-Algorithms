53e3edd57d396c55e95b674a49af0b46845163e7
MaximvanMaanen <maxim.vanmaanen@gmail.com>
2023-11-20 21:45:57 +0100

    Added: StackBenchmarks
    They're not very adventurous, but they were eye opening. Gods, that is fast.

--- Algorithms/Algorithms/Program.cs
+++ Algorithms/Algorithms/Program.cs
@@ -2,6 +2,7 @@
 
 using Algorithms.DoublyLinkedList.Benchmarks;
 using Algorithms.DynamicArrays.Benchmarks;
+using Algorithms.Stack.Benchmarks;
 
 public class Program
 {
@@ -10,6 +11,8 @@ public class Program
         //Run_DynamicArrayBenchmarks();
 
         //Run_DoublyLinkedListBenchmarks();
+
+        Run_StackBenchmarks();
     }
 
     private static void Run_DynamicArrayBenchmarks()
@@ -35,5 +38,48 @@ public class Program
         var benchmarkInstance = new DoublyLinkedListBenchmarks();
 
         benchmarkInstance.Remove_Benchmark();
+    }
+
+    private static void Run_StackBenchmarks()
+    {
+        var benchmarkInstance = new StackBenchmarks();
+
+        benchmarkInstance.Push_Benchmark(10);
+        Console.WriteLine();
+        benchmarkInstance.Push_Benchmark(100);
+        Console.WriteLine();
+        benchmarkInstance.Push_Benchmark(1000);
+        Console.WriteLine();
+        benchmarkInstance.Push_Benchmark(10000);
+        Console.WriteLine();
+        benchmarkInstance.Push_Benchmark(100000);
+        Console.WriteLine();
+        benchmarkInstance.Push_Benchmark(1000000);
+        Console.WriteLine();
+        benchmarkInstance.Push_Benchmark(10000000);
+        Console.WriteLine();
+        benchmarkInstance.Push_Benchmark(100000000);
+        Console.WriteLine();
+        benchmarkInstance.Push_Benchmark(1000000000);
+        Console.WriteLine();
+
+        benchmarkInstance.Pop_Benchmark(10);
+        Console.WriteLine();
+        benchmarkInstance.Pop_Benchmark(100);
+        Console.WriteLine();
+        benchmarkInstance.Pop_Benchmark(1000);
+        Console.WriteLine();
+        benchmarkInstance.Pop_Benchmark(10000);
+        Console.WriteLine();
+        benchmarkInstance.Pop_Benchmark(100000);
+        Console.WriteLine();
+        benchmarkInstance.Pop_Benchmark(1000000);
+        Console.WriteLine();
+        benchmarkInstance.Pop_Benchmark(10000000);
+        Console.WriteLine();
+        benchmarkInstance.Pop_Benchmark(100000000);
+        Console.WriteLine();
+        benchmarkInstance.Pop_Benchmark(1000000000);
+        Console.WriteLine();
     }
 }
--- Algorithms/Algorithms/Stack/Benchmarks/StackBenchmarks.cs
+++ Algorithms/Algorithms/Stack/Benchmarks/StackBenchmarks.cs
@@ -1,7 +1,42 @@
-﻿namespace Algorithms.Stack.Benchmarks
+﻿using Algorithms.Shared;
+using System.Diagnostics;
+
+namespace Algorithms.Stack.Benchmarks
 {
     public class StackBenchmarks
     {
-        //TODO: Figure out what we're going to benchmark here...
+        public void Push_Benchmark(int size)
+        {
+            Console.WriteLine($"Starting push benchmark with {size} items.");
+            Stack<int> stack = new();
+            var sw = new Stopwatch();
+
+            sw.Start();
+            for (int i = 0; i < size; i++)
+            {
+                stack.Push(i);
+            }
+            sw.Stop();
+
+            Console.WriteLine($"Size: {size} - Elapsed time: {sw.Elapsed}");
+        }
+
+        public void Pop_Benchmark(int size)
+        {
+            Console.WriteLine($"Starting pop benchmark with {size} items.");
+            Stack<int> stack = new(DataSetHelper.CreateDataSet(size));
+            var stackSize = stack.Size();
+
+            var sw = new Stopwatch();
+
+            sw.Start();
+            for (int i = 0; i < stackSize; i++)
+            {
+                stack.Pop();
+            }
+            sw.Stop();
+
+            Console.WriteLine($"Size: {size} - Elapsed time: {sw.Elapsed}");
+        }
     }
 }
