c69b94a2e7ed5363f119d5aea12f5acfd9fe9e43
MaximvanMaanen <maxim.vanmaanen@gmail.com>
2023-11-21 21:38:05 +0100

    Added: Deque and Priority Queue
    This is just the beginning.

---
+++ Algorithms/Algorithms/Deque/IDeque.cs
@@ -0,0 +1,27 @@
+﻿namespace Algorithms.Deque
+{
+    public interface IDeque
+    {
+        /// <summary>
+        /// Inserts an item on the left side of the queue.
+        /// </summary>
+        public void InsertLeft();
+
+        /// <summary>
+        /// Inserts an item on the right side of the queue.
+        /// </summary>
+        public void InsertRight();
+
+        /// <summary>
+        /// Deletes an item on the left side of the queue.
+        /// </summary>
+        public void DeleteLeft();
+
+        /// <summary>
+        /// Deletes an item on the right side of the queue.
+        /// </summary>
+        public void DeleteRight();
+
+        public int Count { get; }
+    }
+}
---
+++ Algorithms/Algorithms/PriorityQueue/IPriorityQueue.cs
@@ -0,0 +1,12 @@
+﻿namespace Algorithms.PriorityQueue
+{
+    public interface IPriorityQueue
+    {
+        public void Add();
+        public void Peek();
+        public void Poll();
+        public void Insert();
+        public void FindMin();
+        public void DeleteMin();
+    }
+}
