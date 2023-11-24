1b4a3096cbe88030bb124fd8d5f07bf2a4d50b26
Maxim van Maanen <maxim.vanmaanen@gmail.com>
2023-11-24 12:17:01 +0100

    Updated: Benchmarks
    Made them easier to read.

--- Algorithms/Algorithms/Program.cs
+++ Algorithms/Algorithms/Program.cs
@@ -2,13 +2,14 @@
 
 using Algorithms.DoublyLinkedList.Benchmarks;
 using Algorithms.DynamicArrays.Benchmarks;
+using Algorithms.Shared;
 using Algorithms.Stack.Benchmarks;
 
 public class Program
 {
     private static void Main(string[] args)
     {
-        //Run_DynamicArrayBenchmarks();
+        Run_DynamicArrayBenchmarks();
 
         //Run_DoublyLinkedListBenchmarks();
 
@@ -17,60 +18,38 @@ public class Program
 
     private static void Run_DynamicArrayBenchmarks()
     {
+        List<int> benchmarkValues;
         var benchmarkInstance = new DynamicArrayBenchmarks();
 
-        benchmarkInstance.Add_Benchmark(10);
-        Console.WriteLine();
-        benchmarkInstance.Add_Benchmark(100);
-        Console.WriteLine();
-        benchmarkInstance.Add_Benchmark(1000);
-        Console.WriteLine();
-        benchmarkInstance.Add_Benchmark(10000);
-        Console.WriteLine();
-        benchmarkInstance.Add_Benchmark(100000);
-        Console.WriteLine();
-        benchmarkInstance.Add_Benchmark(200000);
-        Console.WriteLine();
-        benchmarkInstance.Add_Benchmark(500000);
-        Console.WriteLine();
-        benchmarkInstance.Add_Benchmark(1000000);
-        Console.WriteLine();
-        benchmarkInstance.Add_Benchmark(10000000);
-        Console.WriteLine();
-        benchmarkInstance.Add_Benchmark(100000000);
-        Console.WriteLine();
+        benchmarkValues = new List<int> { 10, 100, 1000, 10000, 100000, 1000000, 10000000 };
+        BenchmarkHelper.StartBenchmark("Add");
+        foreach (int value in benchmarkValues)
+        {
+            benchmarkInstance.Add_Benchmark(value);
+            Console.WriteLine();
+        }
 
-        benchmarkInstance.ShrinkAndHalve_Benchmark(10);
-        Console.WriteLine();
-        benchmarkInstance.ShrinkAndHalve_Benchmark(100);
-        Console.WriteLine();
-        benchmarkInstance.ShrinkAndHalve_Benchmark(1000);
-        Console.WriteLine();
-        benchmarkInstance.ShrinkAndHalve_Benchmark(10000);
-        Console.WriteLine();
-        benchmarkInstance.ShrinkAndHalve_Benchmark(100000);
-        Console.WriteLine();
-        benchmarkInstance.ShrinkAndHalve_Benchmark(200000);
-        Console.WriteLine();
+        benchmarkValues = new List<int> { 10, 100, 1000, 10000, 100000, 200000 };
+        BenchmarkHelper.StartBenchmark("Shrink and Halve");
+        foreach (int value in benchmarkValues)
+        {
+            benchmarkInstance.ShrinkAndHalve_Benchmark(value);
+            Console.WriteLine();
+        }
     }
 
     private static void Run_DoublyLinkedListBenchmarks()
     {
+        List<int> benchmarkValues;
         var benchmarkInstance = new DoublyLinkedListBenchmarks();
 
-        benchmarkInstance.Remove_Benchmark(10);
-        Console.WriteLine();
-        benchmarkInstance.Remove_Benchmark(100);
-        Console.WriteLine();
-        benchmarkInstance.Remove_Benchmark(1000);
-        Console.WriteLine();
-        benchmarkInstance.Remove_Benchmark(10000);
-        Console.WriteLine();
-        benchmarkInstance.Remove_Benchmark(100000);
-        Console.WriteLine();
-        benchmarkInstance.Remove_Benchmark(1000000);
-        Console.WriteLine();
-        benchmarkInstance.Remove_Benchmark(10000000);
+        benchmarkValues = new List<int> { 10, 100, 1000, 10000, 100000, 1000000, 10000000 };
+        BenchmarkHelper.StartBenchmark("Remove");
+        foreach (int value in benchmarkValues)
+        {
+            benchmarkInstance.Remove_Benchmark(value);
+            Console.WriteLine();
+        }
     }
 
     private static void Run_StackBenchmarks()
@@ -78,23 +57,28 @@ public class Program
         List<int> benchmarkValues;
         var benchmarkInstance = new StackBenchmarks();
 
-        benchmarkValues = new List<int> { 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000 };
+        benchmarkValues = new List<int> { 10, 100, 1000, 10000, 100000, 1000000, 10000000 };
+        BenchmarkHelper.StartBenchmark("Push");
         foreach (int value in benchmarkValues)
         {
             benchmarkInstance.Push_Benchmark(value);
             Console.WriteLine();
         }
 
-        benchmarkValues = new List<int> { 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000 };
+        benchmarkValues = new List<int> { 10, 100, 1000, 10000, 100000, 1000000, 10000000 };
+        BenchmarkHelper.StartBenchmark("Pop");
         foreach (var item in benchmarkValues)
         {
             benchmarkInstance.Pop_Benchmark(item);
+            Console.WriteLine();
         }
 
-        benchmarkValues = new List<int> { 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000 };
+        benchmarkValues = new List<int> { 10, 100, 1000, 10000, 100000, 1000000, 10000000 };
+        BenchmarkHelper.StartBenchmark("Top");
         foreach (var item in benchmarkValues)
         {
             benchmarkInstance.Top_Benchmark(item);
+            Console.WriteLine();
         }
     }
 }
---
+++ Algorithms/Algorithms/Shared/BenchmarkHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace Algorithms.Shared
+{
+    public static class BenchmarkHelper
+    {
+        public static void StartBenchmark(string benchmarkName)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"-== Starting {benchmarkName} Benchmark ==-");
+        }
+    }
+}
