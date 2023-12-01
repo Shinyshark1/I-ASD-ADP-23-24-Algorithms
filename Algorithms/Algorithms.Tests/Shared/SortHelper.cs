0249d24ff037a497b0f6d5c19d2931b62f11bc2a
MaximvanMaanen <maxim.vanmaanen@gmail.com>
2023-12-01 11:06:26 +0100

    Added: SortHelper
    This will have methods to help with sorting.

---
+++ Algorithms/Algorithms.Tests/Shared/SortHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace Algorithms.Tests.Shared
+{
+    public static class SortHelper
+    {
+        public static bool IsSorted(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                // If our current index is larger than our next index, the sort was unsuccesful.
+                if (array[i] > array[i + 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
