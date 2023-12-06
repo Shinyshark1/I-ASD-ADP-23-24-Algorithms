555b621a0760630bf0475472f7b75b4d1c568c65
MaximvanMaanen <maxim.vanmaanen@gmail.com>
2023-12-06 19:44:05 +0100

    Added: Data test for insertion sort

--- Algorithms/Algorithms.Tests/InsertionSortTests.cs
+++ Algorithms/Algorithms.Tests/InsertionSortTests.cs
@@ -1,10 +1,45 @@
-﻿using Algorithms.Shared;
+﻿using Algorithms.JsonData;
+using Algorithms.JsonData.Sorteren.Models;
+using Algorithms.Shared;
 using Algorithms.Tests.Shared;
+using Newtonsoft.Json;
 
 namespace Algorithms.Tests
 {
     public class InsertionSortTests
     {
+        [Fact]
+        public void InsertionSort_CanSort_IntArrays()
+        {
+            var sortingJson = JsonConstants.ReadDataSetSorting();
+
+            // Arrange & Act
+            var firstArray = InsertionSort.InsertionSort.Sort(JsonConvert.DeserializeObject<LijstAflopend2>(sortingJson).Content);
+            var secondArray = InsertionSort.InsertionSort.Sort(JsonConvert.DeserializeObject<LijstOplopend2>(sortingJson).Content);
+            var fourthArray = InsertionSort.InsertionSort.Sort(JsonConvert.DeserializeObject<LijstGesorteerdAflopend3>(sortingJson).Content);
+            var fifthArray = InsertionSort.InsertionSort.Sort(JsonConvert.DeserializeObject<LijstGesorteerdOplopend3>(sortingJson).Content);
+            var sixthArray = InsertionSort.InsertionSort.Sort(JsonConvert.DeserializeObject<LijstHerhaald1000>(sortingJson).Content);
+            var seventhArray = InsertionSort.InsertionSort.Sort(JsonConvert.DeserializeObject<LijstLeeg0>(sortingJson).Content);
+            var tenthArray = InsertionSort.InsertionSort.Sort(JsonConvert.DeserializeObject<LijstOplopend10000>(sortingJson).Content);
+            var eleventhArray = InsertionSort.InsertionSort.Sort(JsonConvert.DeserializeObject<LijstWillekeurig10000>(sortingJson).Content);
+            var twelfthArray = InsertionSort.InsertionSort.Sort(JsonConvert.DeserializeObject<LijstWillekeurig3>(sortingJson).Content);
+
+            //var thirdArray = InsertionSort.InsertionSort.Sort(JsonConvert.DeserializeObject<LijstFloat8001>(sortingJson).Content);    | double[]
+            //var eighthArray = InsertionSort.InsertionSort.Sort(JsonConvert.DeserializeObject<LijstNull1>(sortingJson).Content);       | int?[]
+            //var ninthArray = InsertionSort.InsertionSort.Sort(JsonConvert.DeserializeObject<LijstNull3>(sortingJson).Content);        | int?[]
+
+            // Assert
+            Assert.True(SortHelper.IsSorted(firstArray));
+            Assert.True(SortHelper.IsSorted(secondArray));
+            Assert.True(SortHelper.IsSorted(fourthArray));
+            Assert.True(SortHelper.IsSorted(fifthArray));
+            Assert.True(SortHelper.IsSorted(sixthArray));
+            Assert.True(SortHelper.IsSorted(seventhArray));
+            Assert.True(SortHelper.IsSorted(tenthArray));
+            Assert.True(SortHelper.IsSorted(eleventhArray));
+            Assert.True(SortHelper.IsSorted(twelfthArray));
+        }
+
         [Fact]
         public void InsertionSort_Sort_SortsTheArrays()
         {
