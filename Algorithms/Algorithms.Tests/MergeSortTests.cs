b27c2938760541ca844b31c37260a5cb4a6efbc1
MaximvanMaanen <maxim.vanmaanen@gmail.com>
2023-12-06 19:59:58 +0100

    Added: MergeSort data test

--- Algorithms/Algorithms.Tests/MergeSortTests.cs
+++ Algorithms/Algorithms.Tests/MergeSortTests.cs
@@ -1,10 +1,45 @@
-﻿using Algorithms.Shared;
+﻿using Algorithms.JsonData;
+using Algorithms.JsonData.Sorteren.Models;
+using Algorithms.Shared;
 using Algorithms.Tests.Shared;
+using Newtonsoft.Json;
 
 namespace Algorithms.Tests
 {
     public class MergeSortTests
     {
+        [Fact]
+        public void MergeSort_CanSort_IntArrays()
+        {
+            var sortingJson = JsonConstants.ReadDataSetSorting();
+
+            // Arrange & Act
+            var firstArray = MergeSort.MergeSort.Sort(JsonConvert.DeserializeObject<LijstAflopend2>(sortingJson).Content);
+            var secondArray = MergeSort.MergeSort.Sort(JsonConvert.DeserializeObject<LijstOplopend2>(sortingJson).Content);
+            var fourthArray = MergeSort.MergeSort.Sort(JsonConvert.DeserializeObject<LijstGesorteerdAflopend3>(sortingJson).Content);
+            var fifthArray = MergeSort.MergeSort.Sort(JsonConvert.DeserializeObject<LijstGesorteerdOplopend3>(sortingJson).Content);
+            var sixthArray = MergeSort.MergeSort.Sort(JsonConvert.DeserializeObject<LijstHerhaald1000>(sortingJson).Content);
+            var seventhArray = MergeSort.MergeSort.Sort(JsonConvert.DeserializeObject<LijstLeeg0>(sortingJson).Content);
+            var tenthArray = MergeSort.MergeSort.Sort(JsonConvert.DeserializeObject<LijstOplopend10000>(sortingJson).Content);
+            var eleventhArray = MergeSort.MergeSort.Sort(JsonConvert.DeserializeObject<LijstWillekeurig10000>(sortingJson).Content);
+            var twelfthArray = MergeSort.MergeSort.Sort(JsonConvert.DeserializeObject<LijstWillekeurig3>(sortingJson).Content);
+
+            //var thirdArray = MergeSort.MergeSort.Sort(JsonConvert.DeserializeObject<LijstFloat8001>(sortingJson).Content);    | double[]
+            //var eighthArray = MergeSort.MergeSort.Sort(JsonConvert.DeserializeObject<LijstNull1>(sortingJson).Content);       | int?[]
+            //var ninthArray = MergeSort.MergeSort.Sort(JsonConvert.DeserializeObject<LijstNull3>(sortingJson).Content);        | int?[]
+
+            // Assert
+            Assert.True(SortHelper.IsSorted(firstArray));
+            Assert.True(SortHelper.IsSorted(secondArray));
+            Assert.True(SortHelper.IsSorted(fourthArray));
+            Assert.True(SortHelper.IsSorted(fifthArray));
+            Assert.True(SortHelper.IsSorted(sixthArray));
+            Assert.True(SortHelper.IsSorted(seventhArray));
+            Assert.True(SortHelper.IsSorted(tenthArray));
+            Assert.True(SortHelper.IsSorted(eleventhArray));
+            Assert.True(SortHelper.IsSorted(twelfthArray));
+        }
+
         [Fact]
         public void InsertionSort_Sort_SortsTheArrays()
         {
