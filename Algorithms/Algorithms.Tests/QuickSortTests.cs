a1493d05be7911cf95a77d4b2ad5d1bd3cf797a3
MaximvanMaanen <maxim.vanmaanen@gmail.com>
2023-12-08 13:10:38 +0100

    Added: QuickSort

---
+++ Algorithms/Algorithms.Tests/QuickSortTests.cs
@@ -0,0 +1,62 @@
+﻿using Algorithms.JsonData;
+using Algorithms.JsonData.Sorteren.Models;
+using Algorithms.Shared;
+using Algorithms.Tests.Shared;
+using Newtonsoft.Json;
+
+namespace Algorithms.Tests
+{
+    public class QuickSortTests
+    {
+        [Fact]
+        public void QuickSort_CanSort_IntArrays()
+        {
+            var sortingJson = JsonConstants.ReadDataSetSorting();
+
+            // Arrange & Act
+            var firstArray = QuickSort.QuickSort.Sort(JsonConvert.DeserializeObject<LijstAflopend2>(sortingJson).Content);
+            var secondArray = QuickSort.QuickSort.Sort(JsonConvert.DeserializeObject<LijstOplopend2>(sortingJson).Content);
+            var fourthArray = QuickSort.QuickSort.Sort(JsonConvert.DeserializeObject<LijstGesorteerdAflopend3>(sortingJson).Content);
+            var fifthArray = QuickSort.QuickSort.Sort(JsonConvert.DeserializeObject<LijstGesorteerdOplopend3>(sortingJson).Content);
+            var sixthArray = QuickSort.QuickSort.Sort(JsonConvert.DeserializeObject<LijstHerhaald1000>(sortingJson).Content);
+            var seventhArray = QuickSort.QuickSort.Sort(JsonConvert.DeserializeObject<LijstLeeg0>(sortingJson).Content);
+            var tenthArray = QuickSort.QuickSort.Sort(JsonConvert.DeserializeObject<LijstOplopend10000>(sortingJson).Content);
+            var eleventhArray = QuickSort.QuickSort.Sort(JsonConvert.DeserializeObject<LijstWillekeurig10000>(sortingJson).Content);
+            var twelfthArray = QuickSort.QuickSort.Sort(JsonConvert.DeserializeObject<LijstWillekeurig3>(sortingJson).Content);
+
+            //var thirdArray = QuickSort.QuickSort.Sort(JsonConvert.DeserializeObject<LijstFloat8001>(sortingJson).Content);    | double[]
+            //var eighthArray = QuickSort.QuickSort.Sort(JsonConvert.DeserializeObject<LijstNull1>(sortingJson).Content);       | int?[]
+            //var ninthArray = QuickSort.QuickSort.Sort(JsonConvert.DeserializeObject<LijstNull3>(sortingJson).Content);        | int?[]
+
+            // Assert
+            Assert.True(SortHelper.IsSorted(firstArray));
+            Assert.True(SortHelper.IsSorted(secondArray));
+            Assert.True(SortHelper.IsSorted(fourthArray));
+            Assert.True(SortHelper.IsSorted(fifthArray));
+            Assert.True(SortHelper.IsSorted(sixthArray));
+            Assert.True(SortHelper.IsSorted(seventhArray));
+            Assert.True(SortHelper.IsSorted(tenthArray));
+            Assert.True(SortHelper.IsSorted(eleventhArray));
+            Assert.True(SortHelper.IsSorted(twelfthArray));
+        }
+
+        [Fact]
+        public void QuickSort_Sort_SortsTheArrays()
+        {
+            // Arrange
+            int[] firstArray = DataSetHelper.CreateRandomDataSet(20);
+            int[] secondArray = DataSetHelper.CreateRandomDataSet(20);
+            int[] thirdArray = DataSetHelper.CreateRandomDataSet(20);
+
+            //Act
+            QuickSort.QuickSort.Sort(firstArray);
+            QuickSort.QuickSort.Sort(secondArray);
+            QuickSort.QuickSort.Sort(thirdArray);
+
+            //Assert
+            Assert.True(SortHelper.IsSorted(firstArray));
+            Assert.True(SortHelper.IsSorted(secondArray));
+            Assert.True(SortHelper.IsSorted(thirdArray));
+        }
+    }
+}
---
+++ Algorithms/Algorithms/QuickSort/QuickSort.cs
@@ -0,0 +1,90 @@
+﻿namespace Algorithms.QuickSort
+{
+    public class QuickSort
+    {
+        public static int[] Sort(int[] array)
+        {
+            InternalQuickSort(array, 0, array.Length - 1);
+            return array;
+        }
+
+        // This works in the same way as the Merge Sort.
+        private static int[] InternalQuickSort(int[] array, int low, int high)
+        {
+            if (low < high)
+            {
+                int pivotIndex = Partition(array, low, high);
+                InternalQuickSort(array, low, pivotIndex - 1);
+                InternalQuickSort(array, pivotIndex + 1, high);
+            }
+
+            return array;
+        }
+
+        private static int Partition(int[] array, int low, int high)
+        {
+            // We find the pivot index by using our median strategy.
+            int pivotIndex = DeterminePivotIndex(array, low, high);
+
+            // We have to swap our pivot to the end of our current selection so that it is in the right spot.
+            Swap(array, pivotIndex, high);
+
+            // Now that our pivot has been swapped to be in the right position, we can pick it up from our high spot.
+            int pivot = array[high];
+
+            int i = low - 1;
+            for (int j = low; j < high; j++)
+            {
+                // If we find an item that is smaller than our pivot we move it to the low part of our array.
+                // We keep moving our low part up by one each time this happens.
+                if (array[j] < pivot)
+                {
+                    i++;
+                    Swap(array, i, j);
+                }
+            }
+
+            // Now that we're done, we move our pivot behind all the items that were smaller than it and infront of all the items that were larger than it.
+            Swap(array, i + 1, high);
+            return i + 1;
+        }
+
+        private static int DeterminePivotIndex(int[] array, int lowIndex, int highIndex)
+        {
+            int middleIndex = (lowIndex + highIndex) / 2;
+            var median = DetermineMedian(array[lowIndex], array[middleIndex], array[highIndex]);
+
+            if (median == array[lowIndex])
+            {
+                return lowIndex;
+            }
+
+            if (median == array[middleIndex])
+            {
+                return middleIndex;
+            }
+
+            return highIndex;
+        }
+
+        private static void Swap(int[] array, int i, int j)
+        {
+            (array[i], array[j]) = (array[j], array[i]);
+        }
+
+        private static int DetermineMedian(int num1, int num2, int num3)
+        {
+            if ((num2 < num1 && num1 < num3) || (num2 > num1 && num1 > num3))
+            {
+                return num1;
+            }
+
+            if ((num1 < num2 && num2 < num3) || (num1 > num2 && num2 > num3))
+            {
+                return num2;
+            }
+
+            return num3;
+        }
+    }
+}
