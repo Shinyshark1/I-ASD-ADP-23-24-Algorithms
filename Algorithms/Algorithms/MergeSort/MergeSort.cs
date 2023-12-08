17791752cf50e50e522c189af2be9a6ef8b4b31f
MaximvanMaanen <maxim.vanmaanen@gmail.com>
2023-12-08 13:26:08 +0100

    Updated: Parallel invoke syntax

--- Algorithms/Algorithms/MergeSort/MergeSort.cs
+++ Algorithms/Algorithms/MergeSort/MergeSort.cs
@@ -33,16 +33,8 @@
 
                 // Parallelize the recursive calls
                 Parallel.Invoke(
-                    () =>
-                    {
-                        // We merge and sort the start side all the way down.
-                        MergeAndSort(array, temporaryArray, start, middle);
-                    },
-                    () =>
-                    {
-                        // We merge and sort the end side all the way down
-                        MergeAndSort(array, temporaryArray, middle + 1, end);
-                    }
+                    () => MergeAndSort(array, temporaryArray, start, middle),
+                    () => MergeAndSort(array, temporaryArray, middle + 1, end)
                 );
 
                 // When the start side is all the way deconstructed to singular items
