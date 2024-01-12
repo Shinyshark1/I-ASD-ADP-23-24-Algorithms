851b837b281bac743af84546ca29565200bb5660
Shinyshark <maxim.vanmaanen@gmail.com>
2024-01-12 16:13:00 +0100

    Draft: AVL Search Tree
    Hello Marco!

---
+++ Algorithms/Algorithms/AVLSearchTree/AvlSearchTree.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.AVLSearchTree
+{
+    public class AvlSearchTree : IAvlSearchTree
+    {
+        public object Find(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object FindMaximum()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object FindMinimum()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
