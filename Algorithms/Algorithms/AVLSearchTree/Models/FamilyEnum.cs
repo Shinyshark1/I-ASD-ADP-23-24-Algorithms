b734c421669e0584da8a1de885ab73c1183aac25
MaximvanMaanen <maxim.vanmaanen@gmail.com>
2024-01-14 12:35:32 +0100

    Updated: TreeNode
    Added an enum for easier family evaluation. Renamed Height to be Depth and added what should actually be the Height. I've added a Parent node which should make things easier to check at a slight cost of memory.

---
+++ Algorithms/Algorithms/AVLSearchTree/Models/FamilyEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace Algorithms.AVLSearchTree.Models
+{
+    public enum FamilyEnum
+    {
+        NoChildren = 0,
+        OneChild = 1,
+        TwoChildren = 2,
+    }
+}
--- Algorithms/Algorithms/AVLSearchTree/Models/TreeNode.cs
+++ Algorithms/Algorithms/AVLSearchTree/Models/TreeNode.cs
@@ -2,25 +2,28 @@
 {
     public class TreeNode
     {
-        public TreeNode(int value, int height)
+        public TreeNode(int value, int depth)
         {
             Key = value;
             Value = value;
             Left = null;
             Right = null;
-            Height = height;
+            Depth = depth;
         }
 
         // Normally we'd have a key AND a value but we only need to insert ints here.
         // For the sake of simplicity, we treat the key AND the value as seperate entities in-case we'd ever want to implement <T>
         public int Key { get; set; }
         public int Value { get; set; }
-        public int Height { get; set; }
+
+        public int Depth { get; set; }
+        public int Height => GetHeight(this);
+
         public TreeNode? Left { get; set; }
         public TreeNode? Right { get; set; }
+        public TreeNode? Parent { get; set; }
 
-        // Finds the depth of the node by recursively going through the tree.
-        public int Size(TreeNode? node)
+        private int GetHeight(TreeNode? node)
         {
             if (node == null)
             {
@@ -28,11 +31,23 @@
             }
             else
             {
-                return
-                    Size(node.Left) + 1
-                    +
-                    Size(node.Right) + 1;
+                return Math.Max(GetHeight(node.Left), GetHeight(node.Right)) + 1;
             }
+        }
+
+        public FamilyEnum GetFamilySituation()
+        {
+            if (Left == null && Right == null)
+            {
+                return FamilyEnum.NoChildren;
+            }
+
+            if (Left != null && Right != null)
+            {
+                return FamilyEnum.TwoChildren;
+            }
+
+            return FamilyEnum.OneChild;
         }
     }
 }
